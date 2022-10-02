d8202cdc653c0239a116bbb8e4eec03e6c594202
Mauro-Domingues <maurinho.villa@hotmail.com>
2022-10-02 15:20:02 -0300

    Nodemon de volta a devDependences e algumas alterações no readme

--- src/routes/mainRoute.js
+++ src/routes/mainRoute.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 router.get("/", (req, res, next) => {
   try{
-    res.status(200).send("Conexão com o Banco de Dados estabelecida")
+    res.status(200).send(`<h2 style="text-align: center; margin-top: 40px;">Conexão com o Banco de Dados estabelecida</h2>`)
   }catch(error){
     res.status(400).send({
       message: error.message
