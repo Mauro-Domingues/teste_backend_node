083460e92f78e49cbb2f6c5dcf4f62e2d0aaa596
Mauro-Domingues <maurinho.villa@hotmail.com>
2022-10-03 08:05:10 -0300

    testando o cors

--- src/app.js
+++ src/app.js
@@ -7,13 +7,6 @@ const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
-  if (req.method === 'OPTIONS'){
-    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE')
-  }next()
-})
 
 app.use('/', mainRoute)
 app.use('/user', userRoute)
