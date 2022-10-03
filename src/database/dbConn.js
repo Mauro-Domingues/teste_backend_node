9634f0edf1a1a2e398d28f4202b2c6e7f806bc89
Mauro-Domingues <maurinho.villa@hotmail.com>
2022-10-03 19:30:17 -0300

    conexão manual

--- src/database/dbConn.js
+++ src/database/dbConn.js
@@ -1,15 +1,18 @@
 const mysql = require('mysql2/promise')
-require('dotenv').config()
-
+/*require('dotenv').config()
+DB_HOST=db4free.net
+DB_USER=sota_admin
+DB_PASSWORD=861928493b3bdd5aa35f5e9b9ec3c6292ff42ee6e60ebc2c36c3a411d5d64cbd
+DB_DATABASE=report_ticket_db*/
 const connectToMySql = async () => {
     if (global.connection && global.connection.state !== 'disconnected'){
         return global.connection
     }
     const config = {
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE
+        host: "db4free.net",
+        user: "sota_admin",
+        password: "861928493b3bdd5aa35f5e9b9ec3c6292ff42ee6e60ebc2c36c3a411d5d64cbd",
+        database: "report_ticket_db"
     }
     const connection = await mysql.createConnection(config)
     global.connection = connection
@@ -17,5 +20,4 @@ const connectToMySql = async () => {
 
 connectToMySql()
 
-const connSuccessful = {connectToMySql}
-module.exports = connSuccessful
+module.exports = {connectToMySql}
