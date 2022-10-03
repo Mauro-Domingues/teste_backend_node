7704bbf198f312112c553cfef246f1edb853e258
Mauro-Domingues <maurinho.villa@hotmail.com>
2022-10-03 19:17:07 -0300

    teste 7

--- src/database/dbConn.js
+++ src/database/dbConn.js
@@ -5,13 +5,13 @@ const connectToMySql = async () => {
     if (global.connection && global.connection.state !== 'disconnected'){
         return global.connection
     }
-    const config = {
+    const connData = {
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_DATABASE
     }
-    const connection = await mysql.createConnection(config)
+    const connection = await mysql.createConnection(connData)
     global.connection = connection
 }
 
