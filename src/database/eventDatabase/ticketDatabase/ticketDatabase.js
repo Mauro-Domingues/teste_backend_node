6f3031383767d54b9a2a8cce0420bea3c44a73b3
Mauro-Domingues <maurinho.villa@hotmail.com>
2022-10-03 19:13:25 -0300

    teste 7

--- src/database/eventDatabase/ticketDatabase/ticketDatabase.js
+++ src/database/eventDatabase/ticketDatabase/ticketDatabase.js
@@ -1,5 +1,5 @@
 const db = require('../../dbConn.js')
-//require('dotenv').config()
+require('dotenv').config()
 
 async function setLot(id, type) {
     const amountPerlot = process.env.INGRESSOS_POR_LOTE
