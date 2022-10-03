dfc480b0e368b2b84c243dc9a128dd35b7e40190
Mauro-Domingues <maurinho.villa@hotmail.com>
2022-10-03 19:43:29 -0300

    config retornada

--- src/database/dbConn.js
+++ src/database/dbConn.js
@@ -1,14 +1,19 @@
 const mysql = require('mysql2/promise')
-const config = require('./dbConfig.js')
+require('dotenv').config()
 
 const connectToMySql = async () => {
     if (global.connection && global.connection.state !== 'disconnected'){
         return global.connection
+    }
+    const config = {
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE
     }
     const connection = await mysql.createConnection(config)
     global.connection = connection
 }
 
 connectToMySql()
-
 module.exports = {connectToMySql}
