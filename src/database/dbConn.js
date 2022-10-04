3c160aa6ca3fe29769869fd39502d607bc95b546
Mauro-Domingues <maurinho.villa@hotmail.com>
2022-10-03 21:28:48 -0300

    teste atualizar variáveis de ambiente

--- src/database/dbConn.js
+++ src/database/dbConn.js
@@ -1,15 +1,20 @@
 const mysql = require('mysql2/promise')
-require('dotenv').config()
+require('dotenv').config() //Por algum motivo místico o heroku está brigando com esse import específico
+
+const host = (process.env.DB_HOST || 'localhost')
+const user = (process.env.DB_USER || 'root')
+const password = (process.env.DB_PASSWORD || '')
+const database = (process.env.DB_DATABASE || 'report_ticket_db')
 
 const connectToMySql = async () => {
     if (global.connection && global.connection.state !== 'disconnected'){
         return global.connection
     }
     const config = {
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE
+        host: host,
+        user: user,
+        password: password,
+        database: database
     }
     const connection = await mysql.createConnection(config)
     global.connection = connection
