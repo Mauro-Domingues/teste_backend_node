3f3a4b894ed1351d082999be9645c2aad56126ac
Mauro-Domingues <maurinho.villa@hotmail.com>
2022-10-03 21:32:28 -0300

    variaveis manuais para deploy

--- src/database/dbConn.js
+++ src/database/dbConn.js
@@ -1,20 +1,15 @@
 const mysql = require('mysql2/promise')
-require('dotenv').config() //Por algum motivo místico o heroku está brigando com esse import específico
-
-const host = (process.env.DB_HOST || 'localhost')
-const user = (process.env.DB_USER || 'root')
-const password = (process.env.DB_PASSWORD || '')
-const database = (process.env.DB_DATABASE || 'report_ticket_db')
+//require('dotenv').config() Por algum motivo místico o heroku está brigando com esse import específico
 
 const connectToMySql = async () => {
     if (global.connection && global.connection.state !== 'disconnected'){
         return global.connection
     }
     const config = {
-        host: host,
-        user: user,
-        password: password,
-        database: database
+        host: 'db4free.net',
+        user: 'sota_admin',
+        password: '861928493b3bdd5aa35f5e9b9ec3c6292ff42ee6e60ebc2c36c3a411d5d64cbd',
+        database: 'report_ticket_db'
     }
     const connection = await mysql.createConnection(config)
     global.connection = connection
